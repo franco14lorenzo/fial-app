20b1a5f72145a4014e6a4c14e74efdeed575885e
Franco Lorenzo <franco14lorenzo@gmail.com>
2023-02-04 03:07:12 -0300

    change home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,8 +9,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="h-full w-full">
-        <h1 className="font-bold text-red-500">Fial App</h1>
+      <main className="grid w-screen h-screen place-content-center">
+        <h1 className="mx-2 my-4 font-bold text-center text-red-500">
+          Fial App
+        </h1>
       </main>
     </>
   )
