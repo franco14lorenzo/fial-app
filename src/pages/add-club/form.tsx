c420b4cfa7de9be80b869bd4d9a01b6827ccb6e6
Franco Lorenzo <franco14lorenzo@gmail.com>
2023-02-12 18:22:41 -0300

    feat: add club form

--- src/pages/add-club/form.tsx
+++ src/pages/add-club/form.tsx
@@ -4,8 +4,9 @@ import { authOptions } from '../api/auth/[...nextauth]'
 import Head from 'next/head'
 import HeaderBar from '@/components/Layout/HeaderBar'
 import AddClubForm from '@/components/Screens/AddClub/AddClubForm'
+import prisma from '@/lib/prisma'
 
-export default function AddClubPage() {
+export default function AddClubPage({ userId }: { userId: string }) {
   return (
     <>
       <Head>
@@ -18,14 +19,14 @@ export default function AddClubPage() {
       <HeaderBar />
 
       <main className="w-full h-full mx-auto max-w-7xl pt-[63px] min-h-screen flex flex-col md:flex-row items-center justify-start gap-10 text-sm px-6">
-        <div className="w-full md:w-1/2 flex flex-col items-start justify-start gap-4 py-5">
-          <h1 className="text-3xl md:text-5xl font-bold text-slate-700 w-full mt-5 md:mt-0">
+        <div className="flex flex-col items-start justify-start w-full gap-4 py-5 md:w-1/2">
+          <h1 className="w-full mt-5 text-3xl font-bold md:text-5xl text-slate-700 md:mt-0">
             Suma tu Club.
           </h1>
-          <h2 className=" text-emerald-600 font-bold text-3xl md:text-5xl w-full">
+          <h2 className="w-full text-3xl font-bold text-emerald-600 md:text-5xl">
             Completa el formulario para agregar tu club.
           </h2>
-          <AddClubForm />
+          <AddClubForm userId={userId} />
         </div>
       </main>
     </>
@@ -35,8 +36,21 @@ export default function AddClubPage() {
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getServerSession(context.req, context.res, authOptions)
 
-  if (!session) {
-    return { redirect: { destination: '/auth/signin' } }
+  if (session) {
+    const user = await prisma.user.findUnique({
+      where: { email: session?.user?.email as string }
+    })
+
+    return { props: { userId: user?.id } }
   }
-  return { props: {} }
+
+  if (!session) {
+    // redirect to signin page and send a redirect url
+    return {
+      redirect: {
+        destination: `/auth/signin?redirectUrl=${context.resolvedUrl}`,
+        permanent: false
+      }
+    }
+  }
 }
